90b80e4039ddfa5c08e9946d2c0b62afa7b619b8
ryan2135 <89617523+ryan2135@users.noreply.github.com>
2022-02-08 15:53:41 -0800

    Add project files.

---
+++ Rational/RationalDriver.cpp
@@ -0,0 +1,27 @@
+//********************************************************************* 
+// File name:	 RationalDriver.cpp
+// Author:		 CS, Pacific University
+// Date:			 
+// Class:			 CS 250
+// Assignment: Rational Lab
+// Purpose:		 Driver used to test class Rational
+//*********************************************************************
+
+#include <iostream>
+
+using namespace std;
+/**********************************************************************
+Function:		  main
+
+Description:	main driver for the rectangle example.
+							show how each member function in rectangle can be used.
+
+Parameters:		none
+
+Returned:			exit status
+**********************************************************************/
+
+int main () {
+
+	return EXIT_SUCCESS;
+}
